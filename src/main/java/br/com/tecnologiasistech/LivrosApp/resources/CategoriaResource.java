d461d50286df5edb605cf1e80191ae7670887dd3
sistechsoftware <uericlislima@gmail.com>
2022-07-08 15:14:41 -0300

    POST  para Categoria

--- src/main/java/br/com/tecnologiasistech/LivrosApp/resources/CategoriaResource.java
+++ src/main/java/br/com/tecnologiasistech/LivrosApp/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package br.com.tecnologiasistech.LivrosApp.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.tecnologiasistech.LivrosApp.domain.Categoria;
 import br.com.tecnologiasistech.LivrosApp.dtos.CategoriaDTO;
@@ -32,11 +36,19 @@ public class CategoriaResource {
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		
-		List<Categoria> list = categoriaService.findAll()
-;
+		List<Categoria> list = categoriaService.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		
 		return ResponseEntity.ok().body(listDTO);
 	}
 
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = categoriaService.create(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		//return ResponseEntity.created(uri).body(obj);
+		return ResponseEntity.created(uri).build();
+	}
 }
