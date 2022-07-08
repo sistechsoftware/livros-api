b395ad22f6d6def8961989206daf8f9fc48591cd
sistechsoftware <uericlislima@gmail.com>
2022-07-08 14:54:57 -0300

    excecao personalizada

--- src/main/java/br/com/tecnologiasistech/LivrosApp/service/CategoriaService.java
+++ src/main/java/br/com/tecnologiasistech/LivrosApp/service/CategoriaService.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Service;
 
 import br.com.tecnologiasistech.LivrosApp.domain.Categoria;
 import br.com.tecnologiasistech.LivrosApp.repositories.CategoriaRepository;
+import br.com.tecnologiasistech.LivrosApp.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		
-		return obj.orElse(null);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
